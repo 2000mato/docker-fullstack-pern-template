61c74091b54c49236155270172d16d3ab78b8c12
Mato <billytomasello1@gmail.com>
2023-06-13 21:50:14 -0400

    dockerization complete, working on template components now

---
+++ frontend/src/components/Content.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/public/vite.svg'
+import '../styles/Content.css'
+
+
+function Content() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Content
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/public/vite.svg'
+import '../styles/Header.css'
+
+
+function Header() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Header
